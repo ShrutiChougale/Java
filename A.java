43cbaed119408695cf17b84db3f898da8732d29f
Shruti Chougale <shrutichougale139@gmail.com>
2026-01-17 23:26:47 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,12 @@
+// what will be the op of following pgm
+
+public class A {
+    public static void main(String[] args) {
+        int a = 500, b;
+        if (a >= 600) {
+            b = 40;
+        } else {
+            System.out.println("b and c are not initialized");
+        }
+    }
+}
